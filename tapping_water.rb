5bf1c42c47747b11ae44c1a3c6b7133c41b36e98
vivak <vk2853@gmail.com>
2021-02-17 23:10:06 +0530

    fixing of tapping water issue

--- tapping_water.rb
+++ tapping_water.rb
@@ -1,30 +1,31 @@
-# @param {Integer[]} height
-# @return {Integer}
+# 42. Trapping Rain Water
+# Hard
+
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
 def trap(height)
-    left = [0]
-    right = [0]
+    left = []
+    right = Array.new(11)
     height_index = height.length - 1
     
     for i in (0..height_index) do
-        max_left = [height[i-1], left[i-1]].max
+        max_left = [height[i-1], left[i-1]].max if i!=0
         max_left = 0 if i == 0
         left.push(max_left)
     end   
     
-     for i in (0..height_index) do
-        max_left = [height[i-1], left[i-1]].max 
-        max_left = 0 if i == 0
-        left.push(max_left)
-    end 
-    
-     for i in height_index.downto(0) do
+    for i in height_index.downto(0) do
         max_right = [height[i+1], right[i+1]].max if i != height_index
         max_right = 0 if i == height_index
-        left.push(max_right)
+        right[i] = max_right
     end
     
+    sum = 0  
     for i in (0..height_index) do
-        sum = left[i] + right[i]
+        min_val = [left[i], right[i]].min
+        s = 0
+        s = min_val - height[i] if min_val > height[i]
+        sum = sum + s
     end
     
     return sum
