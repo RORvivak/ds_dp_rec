2e7c53c72e80d138423cbb4057e59f48c2dedd38
vivak <vk2853@gmail.com>
2021-03-17 21:29:12 +0530

    output success

--- perm_2.rb
+++ perm_2.rb
@@ -1,5 +1,14 @@
-# @param {Integer[]} nums
-# @return {Integer[][]}
+# Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
 
 
 def permute_comb(nums, params, result, h)
@@ -22,13 +31,48 @@ def permute_comb(nums, params, result, h)
 end	
 
 def permute_unique(nums)
+    return [nums] if nums.uniq.length == 1
 	h = {}
 	h.default = 0
 	nums.each do |e|
 		h[e]+=1
     end
 
-    permute_comb(nums, [], [], h)
+    permute_comb(nums, [], [], h).select!{|el| el.count == nums.count}
 end
 
-r = permute_unique([1,2,3])
+
+
+
+def permute_comb(nums, params, result, h)
+	return params if params.length == nums.length
+
+	
+    h.keys.each do|e|
+    	if h[e] > 0
+    		params << e
+    		h[e] -= 1
+    		el =  permute_comb(nums, params.clone, result, h)
+    		result << el if !el.empty?
+            params.pop
+            h[e] +=1
+        end    
+    end	
+
+    result	
+
+end	
+
+def permute_unique(nums)
+    return [nums] if nums.uniq.length == 1
+	h = {}
+	h.default = 0
+	nums.each do |e|
+		h[e]+=1
+    end
+
+    permute_comb(nums, [], [], h).select!{|el| el.count == nums.count}
+end
+
+
+p permute_unique([1,2,3])
